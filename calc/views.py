41b3b2f7bc27ab5f8af9f8724e2acac1fef3edc8
mahdi1820 <beneddinemehdi18@gmail.com>
2023-04-09 00:29:40 +0100

    last commit

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render (request,'calc/index.html')
+
+def lic(request):
+    return render (request,'calc/lic.html')
+
+def master(request):
+    return render (request,'calc/mas.html')    
