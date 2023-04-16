e9833b619d8d964c89c193aec8b8b1393eb570ac
mahdi1820 <beneddinemehdi18@gmail.com>
2023-04-16 05:13:51 +0100

    update

--- Web/settings.py
+++ Web/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'home.context_processors.current_path',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
---
+++ home/context_processors.py
@@ -0,0 +1,6 @@
+
+
+def current_path(request):
+    return {
+        'current_path': request.path
+    }
